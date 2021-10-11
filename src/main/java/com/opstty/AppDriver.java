ac83bf6a754c256ae2b78cce9c7b3b459959d6e3
Cimetiere <66840526+Cimetiere@users.noreply.github.com>
2021-10-11 18:08:29 +0200

    Add files via upload

--- src/main/java/com/opstty/AppDriver.java
+++ src/main/java/com/opstty/AppDriver.java
@@ -1,8 +1,12 @@
 package com.opstty;
 
-import com.opstty.job.District;
 import com.opstty.job.WordCount;
 import org.apache.hadoop.util.ProgramDriver;
+import com.opstty.job.Districts_with_trees;
+import com.opstty.job.Existing_species;
+import com.opstty.job.Count_by_kind;
+import com.opstty.job.Max_height_per_kind_of_tree;
+import com.opstty.job.Trees_sorted_by_height;
 
 public class AppDriver {
     public static void main(String argv[]) {
@@ -12,8 +16,17 @@ public class AppDriver {
         try {
             programDriver.addClass("wordcount", WordCount.class,
                     "A map/reduce program that counts the words in the input files.");
-            programDriver.addClass("distinctDistricts", District.class,
-                    "A map/reduce program that returns the distinct districts with trees in a predefined CSV formatting.");
+            programDriver.addClass("Districts_with_trees", Districts_with_trees.class,
+                    "A map/reduce program that returns the list of distinct districts containing trees.");
+            programDriver.addClass("Existing_species", Existing_species.class,
+                    "A map/reduce program that returns the list of existing species of trees.");
+            programDriver.addClass("Count_by_kind", Count_by_kind.class,
+                    "A map/reduce program that calculates the number of trees in each kind.");
+            programDriver.addClass("Max_height_per_kind_of_tree", Max_height_per_kind_of_tree.class,
+                    "A map/reduce program that calculates the height of the tallest tree in each kind.");
+            programDriver.addClass("Trees_sorted_by_height", Trees_sorted_by_height.class,
+                    "A map/reduce program that sorts the trees height from smallest to largest.");
+
             exitCode = programDriver.run(argv);
         } catch (Throwable throwable) {
             throwable.printStackTrace();
