2288b99e971b2e0da2590ab9aa977f94dee8800c
benoit <benoit.cimetiere@efrei.net>
2021-10-11 17:38:27 +0200

    first commit

--- src/main/java/com/opstty/AppDriver.java
+++ src/main/java/com/opstty/AppDriver.java
@@ -1,5 +1,6 @@
 package com.opstty;
 
+import com.opstty.job.District;
 import com.opstty.job.WordCount;
 import org.apache.hadoop.util.ProgramDriver;
 
@@ -11,7 +12,8 @@ public class AppDriver {
         try {
             programDriver.addClass("wordcount", WordCount.class,
                     "A map/reduce program that counts the words in the input files.");
-
+            programDriver.addClass("distinctDistricts", District.class,
+                    "A map/reduce program that returns the distinct districts with trees in a predefined CSV formatting.");
             exitCode = programDriver.run(argv);
         } catch (Throwable throwable) {
             throwable.printStackTrace();
